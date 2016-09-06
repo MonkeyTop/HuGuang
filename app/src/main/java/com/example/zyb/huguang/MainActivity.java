d941b3e4c402a5f712092b7273f64eb49de171fc
zyb <1032461591@qq.com>
2016-09-06 11:04:10 +0800

    huguang

--- app/src/main/java/com/example/zyb/huguang/MainActivity.java
+++ app/src/main/java/com/example/zyb/huguang/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //湖光是大傻瓜
+        //湖光是新疆人
     }
 }
